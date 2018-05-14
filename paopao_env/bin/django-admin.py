cea028d7e147582f180cc8a9f054fa14fe0732c3
L.C.Y <xiaoye_sama@163.com>
2018-05-14 23:19:43 +0800

    2018=5-14 基础项目模版上传

---
+++ paopao_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/xiaoye_sama/Desktop/my_project/paopao_like/PaoPao_Like/paopao_env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
